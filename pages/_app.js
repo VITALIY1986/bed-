e555a4d55a16e441892410698721f9ab69ff2a78
VITALIY1986 <staslego1986@gmail.com>
2022-02-15 21:57:06 +0200

    sad

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Router from 'next/router';
 import NProgress from 'nprogress';
 import client from "../src/components/ApolloClient";
 import { AppProvider } from "../src/components/context/AppContext";
-import { AuthContextProvider } from '../src/components/login-function/auth-context';
+
 NProgress.configure({ showSpinner: false });
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
@@ -12,7 +12,7 @@ Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   
-  return <AuthContextProvider><AppProvider><ApolloProvider client={client}><Component {...pageProps} /></ApolloProvider></AppProvider></AuthContextProvider>
+  return <AppProvider><ApolloProvider client={client}><Component {...pageProps} /></ApolloProvider></AppProvider>
 
 
 
